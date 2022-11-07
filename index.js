77b361204341a6d62310c1f58e0d343a92481e01
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-11-07 15:00:29 -0800

    feature/hono (#1)
    * Convert to hono
    
    * Make the app a module

--- index.js
+++ index.js
@@ -1,64 +1,52 @@
-import { Router } from 'itty-router'
+import { Hono } from "hono"
 
-const router = Router()
+const app = new Hono()
 
-async function validateRequest(req, access) {
-  const expected = await GmodExpress.get(`token:${access}`)
+async function validateRequest(c, access) {
+  const expected = await c.env.GmodExpress.get(`token:${access}`)
 
   return !!expected
 }
 
-async function putData(data) {
+async function putData(c, data) {
   const id = crypto.randomUUID()
-  await GmodExpress.put(`data:${id}`, data.data)
+  await c.env.GmodExpress.put(`data:${id}`, data.data)
 
   return id
 }
 
-router.get("/", async () => {
-  return Response.redirect("https://github.com/CFC-Servers/gm_express", 302);
-})
-
+app.get("/", async () => Response.redirect("https://github.com/CFC-Servers/gm_express", 302));
 
 // Returns two keys, one for the server and one to send to clients
-router.get("/register", async () => {
+app.get("/register", async (c) => {
   const server = crypto.randomUUID()
   const client = crypto.randomUUID()
   const now = Date.now()
 
-  await GmodExpress.put(`token:${server}`, now)
-  await GmodExpress.put(`token:${client}`, now)
+  await c.env.GmodExpress.put(`token:${server}`, now)
+  await c.env.GmodExpress.put(`token:${client}`, now)
 
-  const response = JSON.stringify({ server: server, client: client })
-  return new Response(response, {
-    status: 200,
-    headers: {
-      "content-type": "application/json",
-    },
-  })
+  return c.json({server: server, client: client})
 })
 
-router.get("/:access/:id", async (request) => {
-  const { access, id } = request.params
-  const isValid = await validateRequest(request, access)
+app.get("/:access/:id", async (c) => {
+  const access = c.req.param("access")
+  const id = c.req.param("id")
+  const isValid = await validateRequest(c, access)
 
   if (!isValid) {
     return new Response("", { status: 403 })
   }
 
-  let data = await GmodExpress.get(`data:${id}`)
+  let data = await c.env.GmodExpress.get(`data:${id}`)
 
   // If data starts with multi: then download and combine all pieces
   if (data.startsWith("multi:")) {
     let combined = ""
 
     const pieces = data.substring(6).split(",")
-    for (const id of pieces) {
-      const piece = await GmodExpress.get(`data:${id}`)
-      combined += piece
-    }
-
-    data = combined
+    const promises = pieces.map((piece) => c.env.GmodExpress.get(`data:${piece}`))
+    await Promise.all(promises).then((results) => data = results.join(""))
   }
 
   const response = JSON.stringify({ data: data })
@@ -73,9 +61,9 @@ router.get("/:access/:id", async (request) => {
 
 const maxDataSize = 24 * 1024 * 1024
 
-router.post("/:access", async request => {
-  const { access } = request.params
-  const isValid = await validateRequest(request, access)
+app.post("/:access", async (c) => {
+  const access = c.req.param("access")
+  const isValid = await validateRequest(c, access)
 
   if (!isValid) {
     return new Response("", { status: 403 })
@@ -88,22 +76,25 @@ router.post("/:access", async request => {
 
   if (data.length < maxDataSize) {
     responseId = crypto.randomUUID()
-    await GmodExpress.put(`data:${responseId}`, data)
+    await c.env.GmodExpress.put(`data:${responseId}`, data)
   } else {
     const chunks = []
+    const promises = []
 
     for (let i = 0; i < data.length; i += maxDataSize) {
       const slice = data.slice(i, i + maxDataSize)
       const id = crypto.randomUUID()
-      await GmodExpress.put(`data:${id}`, slice)
+      promises.push(c.env.GmodExpress.put(`data:${id}`, slice))
       chunks.push(id)
     }
+
+    await Promise.all(promises)
 
     responseId = crypto.randomUUID()
     let combined = chunks.join(",")
     combined = `multi:${combined}`
 
-    await GmodExpress.put(`data:${responseId}`, combined)
+    await c.env.GmodExpress.put(`data:${responseId}`, combined)
   }
 
   const response = JSON.stringify({ id: responseId })
@@ -115,6 +106,4 @@ router.post("/:access", async request => {
   })
 })
 
-addEventListener('fetch', (e) => {
-  e.respondWith(router.handle(e.request))
-})
+export default app
