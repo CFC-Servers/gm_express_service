04aa1ad1b3be0ee9691eacb0e75523eff04e9048
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-11-10 16:15:16 -0800

    MVP

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// import { Hono } from "hono"
+import { Hono } from "hono"
 const app = new Hono()
 const expiration = 60 * 60 * 24
-// const maxDataSize = 24 * 1024 * 1024
-const maxDataSize = 100
+const maxDataSize = 24 * 1024 * 1024
 
 const makeMetadata = (c, extraMetadata) => {
   return {
@@ -14,21 +13,20 @@ const makeMetadata = (c, extraMetadata) => {
   }
 }
 
-async function validateRequest(c, access) {
-  const expected = await c.env.GmodExpress.get(`token:${access}`)
+async function validateRequest(c, token) {
+  // TODO: Do a sanity check on expiration time too
+  const expected = await c.env.GmodExpress.get(`token:${token}`)
 
   return !!expected
 }
 
-async function putData(c, data, isMulti) {
-  isMulti = isMulti || false
-
+async function putData(c, data) {
   const id = crypto.randomUUID()
-  const metadata = makeMetadata(c, dataType, { isMulti: isMulti })
+  const metadata = makeMetadata(c)
 
   await Promise.all([
-    c.env.GmodExpress.put( `data:${id}`, data, { ...metadata, type: "arrayBuffer" }),
-    c.env.GmodExpress.put( `size:${id}`, data, metadata)
+    c.env.GmodExpress.put(`size:${id}`, data.byteLength, metadata),
+    c.env.GmodExpress.put(`data:${id}`, data, { ...metadata, type: "arrayBuffer" })
   ])
 
   return id
@@ -47,28 +45,6 @@ async function getSize(c, id) {
   return await c.env.GmodExpress.get(`size:${id}`)
 }
 
-async function splitData(c, data) {
-  const chunks = []
-  const promises = []
-
-  for (let i = 0; i < data.byteLength; i += maxDataSize) {
-    const slice = data.slice(i, i + maxDataSize)
-    promises.push(putData(c, slie))
-  }
-
-  await Promise.all(promises).then((results) => {
-    chunks.push(...results)
-  })
-
-  const responseId = crypto.randomUUID()
-  let combined = chunks.join(",")
-  combined = `multi:${combined}`
-
-  await putData(c, combined)
-
-  return c.json({id: responseId})
-}
-
 async function registerRequest(c) {
   const server = crypto.randomUUID()
   const client = crypto.randomUUID()
@@ -82,56 +58,42 @@ async function registerRequest(c) {
 }
 
 async function readRequest(c) {
-  const access = c.req.param("token")
-  const id = c.req.param("id")
-  const isValid = await validateRequest(c, access)
-
+  const token = c.req.param("token")
+  const isValid = await validateRequest(c, token)
   if (!isValid) {
-    return c.text("", 403)
+    return c.text("", 401)
   }
 
-  let data = await getData(c, id)
-
-  // If data starts with multi: then download and combine all pieces
-  if (typeof(data) === "string" && data.startsWith("multi:")) {
-    console.log("Received a request to download a multi-part file")
-    const pieces = data.substring(6).split(",")
-    const promises = pieces.map((piece) => getData(c, piece))
-    await Promise.all(promises)
-      .then( async (results) => {
-        console.log("Received all pieces, combining them", results.length)
-        data = await new Blob(results).arrayBuffer()
-      })
-  }
+  const id = c.req.param("id")
+  const data = await getData(c, id)
   return c.body(data, 200, { "Content-Type": "application/octet-stream" })
 }
 
 async function readSizeRequest(c) {
-  const access = c.req.param("token")
-  const id = c.req.param("id")
-  const isValid = await validateRequest(c, access)
+  const token = c.req.param("token")
+  const isValid = await validateRequest(c, token)
+  if (!isValid) {
+    return c.text("", 401)
+  }
 
+  const id = c.req.param("id")
   return c.json({size: await getSize(c, id)})
 }
 
 async function writeRequest(c) {
-  const access = c.req.param("token")
-  const isValid = await validateRequest(c, access)
-
+  const token = c.req.param("token")
+  const isValid = await validateRequest(c, token)
   if (!isValid) {
-    return c.text("", 403)
+    return c.text("", 401)
   }
 
   const data = await c.req.arrayBuffer()
-
-  if (data.byteLength < maxDataSize) {
-    const id = await putData(c, data)
-    return c.json({id: id})
+  if (data.byteLength > maxDataSize) {
+    return c.text("Data exceeds maximum size of " + maxDataSize, 413)
   }
 
-  const remoteCloudflareIP = c.req.header("CF-Connecting-IP")
-  console.log("Received a request to upload a large file", remoteCloudflareIP, data.byteLength)
-  return await splitData(c, data)
+  const id = await putData(c, data)
+  return c.json({id: id})
 }
 
 app.get("/", async () => Response.redirect("https://github.com/CFC-Servers/gm_express", 302));
@@ -142,6 +104,5 @@ app.get("/v1/read/:token/:id", readRequest)
 app.get("/v1/size/:token/:id", readSizeRequest)
 app.post("/v1/write/:token", writeRequest)
 app.get("/v1/revision", async (c) => c.json({revision: 1}))
-
 
 export default app
