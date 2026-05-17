6938a47bbb9f43a6b3316a45847311b7985f39a8
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2026-05-18 01:27:04 +0200

    Refactor LRUCache implementation to handle token-specific storage

--- docker/selfhost.js
+++ docker/selfhost.js
@@ -4,15 +4,22 @@ import { logger } from "https://deno.land/x/hono@v3.2.7/middleware/logger/index.
 import LRUCache from "https://deno.land/x/lru_cache@6.0.0-deno.4/mod.ts"
 
 const expiration = parseInt(Deno.env.get("GM_EXPRESS_EXPIRATION") || "") || 5 * 60
+const tokenExpiration = 24 * 60 * 60
 const store = new LRUCache({ maxAge: expiration * 1000, max: 1024 })
+const tokenStore = new LRUCache({ maxAge: tokenExpiration * 1000, max: 4096 })
 
 const expressStub = {
   get: async (key, _metadata) => {
+    if (key.startsWith("token:")) return tokenStore.get(key)
     return store.get(key)
   },
 
   put: async (key, value, _metadata) => {
-    store.set(key, value)
+    if (key.startsWith("token:")) {
+      tokenStore.set(key, value)
+    } else {
+      store.set(key, value)
+    }
   }
 }
 
