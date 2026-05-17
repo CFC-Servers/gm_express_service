e2e8b04e8cdbf0ac00b8a0e11b569a0e471cd828
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2026-05-17 21:56:20 +0200

    Remove KeyDB, implement LRU cache instead

--- docker/selfhost.js
+++ docker/selfhost.js
@@ -1,31 +1,19 @@
 import { Hono } from "https://deno.land/x/hono@v3.2.7/mod.ts"
-import { connect } from "https://deno.land/x/redis@v0.27.4/mod.ts"
 import { serve } from "https://deno.land/std@0.167.0/http/server.ts"
 import { compress } from "https://deno.land/x/hono@v3.2.7/middleware/compress/index.ts"
 import { logger } from "https://deno.land/x/hono@v3.2.7/middleware/logger/index.ts"
+import LRUCache from "https://deno.land/x/lru_cache@6.0.0-deno.4/mod.ts"
 
-const expiration = 60 * 60 * 24
-const redis = await connect({ hostname: "kv" })
+const TTL_DETAULT = 5 * 60 * 1000 // 5 minutes
+const store = new LRUCache({ maxAge: TTL_DETAULT })
 
 const expressStub = {
-  get: async (key, metadata) => {
-    const isBuffer = metadata?.type === "arrayBuffer"
-    const data = await redis.executor.exec("GET", key)
-    return isBuffer ? data.buffer() : data
+  get: async (key, _metadata) => {
+    return store.get(key)
   },
 
-  put: async (key, value, metadata) => {
-    let ttl = expiration * 2
-    if (metadata) {
-      if (metadata.expirationTtl) {
-        ttl = metadata.expirationTtl
-      }
-      if (metadata.type === "arrayBuffer") {
-        value = new Uint8Array(value)
-      }
-    }
-
-    await redis.sendCommand("SET", key, value, "EX", ttl)
+  put: async (key, value, _metadata) => {
+    store.set(key, value)
   }
 }
 
