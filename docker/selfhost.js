dc5fbc666a3141d88a4a5972a9f0b461bc1059f6
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2026-05-18 00:51:09 +0200

    Remove unused compress middleware import from selfhost.js

--- docker/selfhost.js
+++ docker/selfhost.js
@@ -1,6 +1,5 @@
 import { Hono } from "https://deno.land/x/hono@v3.2.7/mod.ts"
 import { serve } from "https://deno.land/std@0.167.0/http/server.ts"
-import { compress } from "https://deno.land/x/hono@v3.2.7/middleware/compress/index.ts"
 import { logger } from "https://deno.land/x/hono@v3.2.7/middleware/logger/index.ts"
 import LRUCache from "https://deno.land/x/lru_cache@6.0.0-deno.4/mod.ts"
 
